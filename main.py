c7d643e60dd9e6e94bb707b61a13ab9ba942a6fb
Victor Avalos Torrez <you@example.com>
2025-07-27 14:31:43 -0400

    función inicio

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ def home():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
