a4c52224904e5526c529ea32f15331dfc54c52d8
Victor Avalos Torrez <you@example.com>
2025-07-27 14:30:09 -0400

    función inicio

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {"message": "Hello World vat"}
+    return {"message": "Hello World vat fast api..."}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=8000)
